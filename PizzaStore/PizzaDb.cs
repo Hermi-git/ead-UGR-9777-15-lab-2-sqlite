9477cb7cb3fe6a7243d55cb6ca970b88def6e9e5
Hermi-git <andargiehermela1@gmail.com>
2025-11-28 19:22:51 +0300

    Add Pizza and PizzaDb classes for database context and model representation

---
+++ PizzaStore/PizzaDb.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace PizzaStore.Models
+{
+    public class PizzaDb : DbContext
+    {
+        public PizzaDb(DbContextOptions<PizzaDb> options) : base(options)
+        {
+        }
+
+        public DbSet<Pizza> Pizzas => Set<Pizza>();
+    }
+}
